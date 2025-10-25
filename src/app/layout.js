bcf40e15e6bca9b7403398f07d1bf3872ed3bc1f
ShahadatHasan623 <shahadathasan743599@gmail.com>
2025-10-25 12:25:17 +0600

    added file structure

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,6 +22,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        layout
         {children}
       </body>
     </html>
