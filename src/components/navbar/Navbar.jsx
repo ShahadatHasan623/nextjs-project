1ad597ed8f831e6f02d7d0d20592c63504ed95ad
ShahadatHasan623 <shahadathasan743599@gmail.com>
2025-10-25 17:56:31 +0600

    added navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,87 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  const link = [
+    {
+      id: 1,
+      title: "Home",
+      url: "/",
+    },
+    {
+      id: 2,
+      title: "Portfolio",
+      url: "/portfolio",
+    },
+    {
+      id: 3,
+      title: "Blog",
+      url: "/blog",
+    },
+    {
+      id: 4,
+      title: "About",
+      url: "/about",
+    },
+    {
+      id: 5,
+      title: "Contact",
+      url: "/contact",
+    },
+    {
+      id: 6,
+      title: "Dashboard",
+      url: "/dashboard",
+    },
+  ];
+  return (
+    <div className="navbar bg-base-100 shadow-sm px-8">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            {link.map((links) => (
+              <li key={links.id}>
+                <Link href={links.url}>{links.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <Link href="/">daisyUI</Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          {link.map((links) => (
+            <li key={links.id}>
+              <Link href={links.url}>{links.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Button</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
